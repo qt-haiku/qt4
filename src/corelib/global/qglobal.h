696f4eb116d1d25261d029ebfee968e9338dd427
Yourself <user@i5.(none)>
2010-04-12 23:13:27 +0000

    Add Haiku support to configure script and global.h. Part 2

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -173,6 +173,7 @@ namespace QT_NAMESPACE {}
      LYNX     - LynxOS
      BSD4     - Any BSD 4.4 system
      UNIX     - Any UNIX BSD/SYSV system
+     HAIKU   - Haiku system
 */
 
 #if defined(__APPLE__) && (defined(__GNUC__) || defined(__xlC__) || defined(__xlc__))
@@ -256,6 +257,8 @@ namespace QT_NAMESPACE {}
 #  define Q_OS_INTEGRITY
 #elif defined(VXWORKS) /* there is no "real" VxWorks define - this has to be set in the mkspec! */
 #  define Q_OS_VXWORKS
+#elif defined(__HAIKU__)
+#  define Q_OS_HAIKU
 #elif defined(__MAKEDEPEND__)
 #else
 #  error "Qt has not been ported to this OS - talk to qt-bugs@trolltech.com"
@@ -783,6 +786,7 @@ namespace QT_NAMESPACE {}
      S60      - Symbian S60
      PM       - unsupported
      WIN16    - unsupported
+     HAIKU	- Haiku, Application Server
 */
 
 #if defined(Q_OS_MSDOS)
@@ -790,6 +794,8 @@ namespace QT_NAMESPACE {}
 #  error "Qt requires Win32 and does not work with Windows 3.x"
 #elif defined(_WIN32_X11_)
 #  define Q_WS_X11
+#elif defined(Q_OS_HAIKU)
+#  define Q_WS_HAIKU
 #elif defined(Q_OS_WIN32)
 #  define Q_WS_WIN32
 #  if defined(Q_OS_WIN64)
@@ -820,6 +826,10 @@ namespace QT_NAMESPACE {}
 #  elif !defined(Q_WS_QWS)
 #    define Q_WS_X11
 #  endif
+#endif
+
+#if defined(Q_OS_HAIKU) && defined (Q_WS_X11)
+#undef Q_WS_X11
 #endif
 
 #if defined(Q_WS_WIN16) || defined(Q_WS_WIN32) || defined(Q_WS_WINCE)
--- src/corelib/global/qnamespace.h
+++ src/corelib/global/qnamespace.h
@@ -1648,6 +1648,8 @@ public:
     typedef void * HANDLE;
 #elif defined(Q_OS_SYMBIAN)
     typedef unsigned long int HANDLE; // equivalent to TUint32
+#elif defined(Q_OS_HAIKU)
+    typedef unsigned long HANDLE; // equivalent to TUint32
 #endif
     typedef WindowFlags WFlags;
 
