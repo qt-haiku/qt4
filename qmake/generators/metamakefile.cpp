a7b95a275005babb09101caa9005492e1353879a
Yourself <user@i5.(none)>
2010-04-13 00:57:27 +0000

    Fixed qmake haiku mode

--- qmake/generators/metamakefile.cpp
+++ qmake/generators/metamakefile.cpp
@@ -497,6 +497,9 @@ MetaMakefileGenerator::modesForGenerator(const QString &gen,
 #ifdef Q_OS_MAC
         *host_mode = Option::HOST_MACX_MODE;
         *target_mode = Option::TARG_MACX_MODE;
+#elif defined(Q_OS_HAIKU)
+        *host_mode = Option::HOST_HAIKU_MODE;
+        *target_mode = Option::TARG_HAIKU_MODE;
 #else
         *host_mode = Option::HOST_UNIX_MODE;
         *target_mode = Option::TARG_UNIX_MODE;
