71e1d8aa07ecb9a6e67ec1dbfab83a5e1338952b
Shade <shade@sibtruboprovod.ru>
2010-04-23 00:22:42 +0000

    FIONREAD on PIPEs is unimplimented in Haiku (yet?). Return fake 1024 bytes as available for reading. Not best solution, but its work...

--- src/corelib/io/qprocess_unix.cpp
+++ src/corelib/io/qprocess_unix.cpp
@@ -792,8 +792,12 @@ qint64 QProcessPrivate::bytesAvailableFromStdout() const
 {
     int nbytes = 0;
     qint64 available = 0;
+#ifdef Q_OS_HAIKU
+	available = 1024;
+#else
     if (::ioctl(stdoutChannel.pipe[0], FIONREAD, (char *) &nbytes) >= 0)
         available = (qint64) nbytes;
+#endif        
 #if defined (QPROCESS_DEBUG)
     qDebug("QProcessPrivate::bytesAvailableFromStdout() == %lld", available);
 #endif
@@ -804,8 +808,12 @@ qint64 QProcessPrivate::bytesAvailableFromStderr() const
 {
     int nbytes = 0;
     qint64 available = 0;
+#ifdef Q_OS_HAIKU
+	available = 1024;
+#else
     if (::ioctl(stderrChannel.pipe[0], FIONREAD, (char *) &nbytes) >= 0)
         available = (qint64) nbytes;
+#endif
 #if defined (QPROCESS_DEBUG)
     qDebug("QProcessPrivate::bytesAvailableFromStderr() == %lld", available);
 #endif
