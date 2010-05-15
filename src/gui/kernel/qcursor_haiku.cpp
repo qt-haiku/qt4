1defc223ac2908ea167c88ac302e6b7d2ff48d3e
Yourself <user@shredder.(none)>
2010-05-16 00:46:15 +1100

    Add qt to native cursor mapping. Cleanup and optimize code.

--- src/gui/kernel/qcursor_haiku.cpp
+++ src/gui/kernel/qcursor_haiku.cpp
@@ -12,6 +12,28 @@ int	haiku_global_mouse_y = 0;
 BCursor *HaikuCursorCache[32]={NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,
 							   NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
 
+int	CursorMapList[]={	Qt::ArrowCursor,		B_CURSOR_ID_SYSTEM_DEFAULT,
+						Qt::UpArrowCursor,		B_CURSOR_ID_RESIZE_NORTH,
+						Qt::CrossCursor,		B_CURSOR_ID_CROSS_HAIR,
+						Qt::WaitCursor,			B_CURSOR_ID_PROGRESS,
+						Qt::IBeamCursor,		B_CURSOR_ID_I_BEAM,
+						Qt::SizeVerCursor,		B_CURSOR_ID_RESIZE_NORTH_SOUTH,
+						Qt::SizeHorCursor,		B_CURSOR_ID_RESIZE_EAST_WEST,
+						Qt::SizeBDiagCursor,	B_CURSOR_ID_RESIZE_NORTH_EAST_SOUTH_WEST,
+						Qt::SizeFDiagCursor,	B_CURSOR_ID_RESIZE_NORTH_WEST_SOUTH_EAST,
+						Qt::SizeAllCursor,		B_CURSOR_ID_MOVE,
+						Qt::BlankCursor,		B_CURSOR_ID_NO_CURSOR,
+						Qt::SplitVCursor,		B_CURSOR_ID_RESIZE_NORTH_SOUTH,
+						Qt::SplitHCursor,		B_CURSOR_ID_RESIZE_EAST_WEST,
+						Qt::PointingHandCursor,	B_CURSOR_ID_FOLLOW_LINK,
+						Qt::ForbiddenCursor,	B_CURSOR_ID_NOT_ALLOWED,
+						Qt::OpenHandCursor,		B_CURSOR_ID_GRAB,
+						Qt::ClosedHandCursor,	B_CURSOR_ID_GRABBING,
+						Qt::WhatsThisCursor,	B_CURSOR_ID_HELP,
+						Qt::BusyCursor,			B_CURSOR_ID_PROGRESS
+};
+
+
 extern QCursorData *qt_cursorTable[Qt::LastCursor + 1]; // qcursor.cpp
 
 QPoint QCursor::pos()
@@ -25,54 +47,33 @@ void QCursor::setPos(int x, int y)
 //	haiku_global_mouse_y = y;
 }
 
-#ifndef QT_NO_CURSOR
 BCursor *QCursor::handle() const
 {
+	int i;
+	
     if (!QCursorData::initialized)
         QCursorData::initialize();
-	if(d->cshape==Qt::IBeamCursor)
-		return HaikuCursorCache[B_CURSOR_ID_I_BEAM];
-	if(d->cshape==Qt::PointingHandCursor)
-		return HaikuCursorCache[B_CURSOR_ID_FOLLOW_LINK];
-	if(d->cshape==Qt::CrossCursor)
-		return HaikuCursorCache[B_CURSOR_ID_CROSS_HAIR];
-	if(d->cshape==Qt::SplitHCursor)
-		return HaikuCursorCache[B_CURSOR_ID_RESIZE_EAST_WEST];
-	if(d->cshape==Qt::SplitVCursor)
-		return HaikuCursorCache[B_CURSOR_ID_RESIZE_NORTH_SOUTH];
-	if(d->cshape==Qt::BlankCursor)
-		return HaikuCursorCache[B_CURSOR_ID_NO_CURSOR];
-	if(d->cshape==Qt::WhatsThisCursor)
-		return HaikuCursorCache[B_CURSOR_ID_HELP];
-		
+	
+	for(i=0;i<sizeof(CursorMapList)/2;i+=2) {
+		if(CursorMapList[i]==d->cshape) {
+			if(HaikuCursorCache[CursorMapList[i+1]])
+				return HaikuCursorCache[CursorMapList[i+1]];
+		}
+	}
+				
 	return (BCursor*)B_CURSOR_SYSTEM_DEFAULT;
 }
-#endif
 
 QCursorData::QCursorData(Qt::CursorShape s)
     : cshape(s), bm(0), bmm(0), hx(0), hy(0)
 {
-	if(s == Qt::IBeamCursor)
-		if(!HaikuCursorCache[B_CURSOR_ID_I_BEAM])
-			HaikuCursorCache[B_CURSOR_ID_I_BEAM] = new BCursor(B_CURSOR_ID_I_BEAM);		
-	if(s == Qt::PointingHandCursor)
-		if(!HaikuCursorCache[B_CURSOR_ID_FOLLOW_LINK])
-			HaikuCursorCache[B_CURSOR_ID_FOLLOW_LINK] = new BCursor(B_CURSOR_ID_FOLLOW_LINK);
-	if(s == Qt::CrossCursor)
-		if(!HaikuCursorCache[B_CURSOR_ID_CROSS_HAIR])
-			HaikuCursorCache[B_CURSOR_ID_CROSS_HAIR] = new BCursor(B_CURSOR_ID_CROSS_HAIR);
-	if(s == Qt::SplitHCursor)
-		if(!HaikuCursorCache[B_CURSOR_ID_RESIZE_EAST_WEST])
-			HaikuCursorCache[B_CURSOR_ID_RESIZE_EAST_WEST] = new BCursor(B_CURSOR_ID_RESIZE_EAST_WEST);
-	if(s == Qt::SplitVCursor)
-		if(!HaikuCursorCache[B_CURSOR_ID_RESIZE_NORTH_SOUTH])
-			HaikuCursorCache[B_CURSOR_ID_RESIZE_NORTH_SOUTH] = new BCursor(B_CURSOR_ID_RESIZE_NORTH_SOUTH);
-	if(s == Qt::BlankCursor)
-		if(!HaikuCursorCache[B_CURSOR_ID_NO_CURSOR])
-			HaikuCursorCache[B_CURSOR_ID_NO_CURSOR] = new BCursor(B_CURSOR_ID_NO_CURSOR);
-	if(s == Qt::WhatsThisCursor)
-		if(!HaikuCursorCache[B_CURSOR_ID_HELP])
-			HaikuCursorCache[B_CURSOR_ID_HELP] = new BCursor(B_CURSOR_ID_HELP);
+	int i;
+	for(i=0;i<sizeof(CursorMapList)/2;i+=2) {
+		if(CursorMapList[i]==s) {
+			if(!HaikuCursorCache[CursorMapList[i+1]])
+				HaikuCursorCache[CursorMapList[i+1]] = new BCursor((BCursorID)CursorMapList[i+1]);					
+		}
+	}
 	
     ref = 1;
 }
