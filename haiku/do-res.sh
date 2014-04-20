2126176815bac09f539f8409839a6bf04fcdb152
waddlesplash <ajcsweb@gmail.com>
2014-04-20 16:54:19 -0400

    Make do-res script actually work.

--- haiku/do-res.sh
+++ haiku/do-res.sh
@@ -2,40 +2,40 @@
 # Assistant
 ######################################################################
 rc assistant.rdef
-xres -o "$QTROOT/boot/common/bin/assistant" assistant.rsrc
-mimeset -f "$QTROOT/boot/common/bin/assistant"
+xres -o "../bin/assistant" assistant.rsrc
+mimeset -f "../bin/assistant"
 
 ######################################################################
 # Designer
 ######################################################################
 rc designer.rdef
-xres -o "$QTROOT/boot/common/bin/designer" designer.rsrc
-mimeset -f "$QTROOT/boot/common/bin/designer"
+xres -o "../bin/designer" designer.rsrc
+mimeset -f "../bin/designer"
 
 ######################################################################
 # Linguist
 ######################################################################
 rc linguist.rdef
-xres -o "$QTROOT/boot/common/bin/linguist" linguist.rsrc
-mimeset -f "$QTROOT/boot/common/bin/linguist"
+xres -o "../bin/linguist" linguist.rsrc
+mimeset -f "../bin/linguist"
 
 ######################################################################
 # Help Conversion Wizard
 ######################################################################
 rc qhelpconverter.rdef
-xres -o "$QTROOT/boot/common/bin/qhelpconverter" qhelpconverter.rsrc
-mimeset -f "$QTROOT/boot/common/bin/qhelpconverter"
+xres -o "../bin/qhelpconverter" qhelpconverter.rsrc
+mimeset -f "../bin/qhelpconverter"
 
 ######################################################################
 # QML Viewer
 ######################################################################
 rc qmlviewer.rdef
-xres -o "$QTROOT/boot/common/bin/qmlviewer" qmlviewer.rsrc
-mimeset -f "$QTROOT/boot/common/bin/qmlviewer"
+xres -o "../bin/qmlviewer" qmlviewer.rsrc
+mimeset -f "../bin/qmlviewer"
 
 ######################################################################
 # Config
 ######################################################################
 rc qtconfig.rdef
-xres -o "$QTROOT/boot/common/bin/qtconfig" qtconfig.rsrc
-mimeset -f "$QTROOT/boot/common/bin/qtconfig"
+xres -o "../bin/qtconfig" qtconfig.rsrc
+mimeset -f "../bin/qtconfig"
