d3307edd4cb897a5349a1d60ffad9ff92a6b3051
Gerasim Troeglazov <3dEyes@gmail.com>
2014-07-26 10:45:48 +0000

    Fix qtwebkit crashes

--- src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h
+++ src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h
@@ -737,15 +737,6 @@
 #endif
 #endif
 
-#if PLATFORM(HAIKU)
-#define HAVE_POSIX_MEMALIGN 1
-#define WTF_USE_CURL 1
-#define WTF_USE_PTHREADS 1
-#define HAVE_PTHREAD_RWLOCK 1
-#define USE_SYSTEM_MALLOC 1
-#define ENABLE_NETSCAPE_PLUGIN_API 0
-#endif
-
 #if PLATFORM(BREWMP)
 #define USE_SYSTEM_MALLOC 1
 #endif
@@ -771,7 +762,7 @@
 #endif
 
 #if !OS(WINDOWS) && !OS(SOLARIS) && !OS(QNX) \
-    && !OS(SYMBIAN) && !OS(HAIKU) && !OS(RVCT) \
+    && !OS(SYMBIAN) && !OS(RVCT) \
     && !OS(ANDROID) && !PLATFORM(BREWMP)
 #define HAVE_TM_GMTOFF 1
 #define HAVE_TM_ZONE 1
@@ -845,6 +836,29 @@
 #define HAVE_SYS_PARAM_H 1
 #define HAVE_SYS_TIME_H 1
 
+#elif OS(HAIKU)
+#define WTF_USE_HAIKU 1
+
+#define HAVE_ERRNO_H 1
+#define HAVE_LANGINFO_H 1
+#define HAVE_MERGESORT 1
+#define HAVE_PTHREAD_RWLOCK 1
+
+#define HAVE_SIGNAL_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_TIMEGM 1
+#define HAVE_TM_GMTOFF 1
+#define HAVE_TM_ZONE 1
+#define HAVE_SYS_TIME_H 1
+
+#define HAVE_MMAP 1
+#define HAVE_POSIX_MEMALIGN 1
+
+#define ENABLE_NETSCAPE_PLUGIN_API 0
+#define ENABLE_INSPECTOR 1
+#define ENABLE_JAVASCRIPT_DEBUGGER 1
+
 #elif OS(ANDROID)
 
 #define HAVE_ERRNO_H 1
@@ -860,10 +874,7 @@
 /* FIXME: is this actually used or do other platforms generate their own config.h? */
 
 #define HAVE_ERRNO_H 1
-/* As long as Haiku doesn't have a complete support of locale this will be disabled. */
-#if !OS(HAIKU)
 #define HAVE_LANGINFO_H 1
-#endif
 #define HAVE_MMAP 1
 #define HAVE_SBRK 1
 #define HAVE_STRINGS_H 1
@@ -877,7 +888,7 @@
 #if PLATFORM(QT)
 /* We must not customize the global operator new and delete for the Qt port. */
 #define ENABLE_GLOBAL_FASTMALLOC_NEW 0
-#if !OS(UNIX) || OS(SYMBIAN)
+#if !OS(UNIX) || OS(SYMBIAN) || OS(HAIKU)
 #define USE_SYSTEM_MALLOC 1
 #endif
 #endif
